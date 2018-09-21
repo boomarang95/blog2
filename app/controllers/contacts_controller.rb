f083a7079ca4eebb47b5ae2ce9de4639a2627777
boomerang95 <kinkstill@bk.ru>
2018-09-21 10:36:56 +0300

    Revert "g contacts"
    This reverts commit ecf004ce7d4a36f6fa3114428553a68d2d235820.

--- app/controllers/contacts_controller.rb
+++
@@ -1,9 +0,0 @@
-class ContactsController < ApplicationController
-
-  def new
-  end
-
-  def create
-  end
-  
-end
--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,6 @@ Rails.application.routes.draw do
   get 'home/index'
 
   resources :articles
-  resources :contacts, only: [:new, :create]
 
   root 'home#index'
 end
